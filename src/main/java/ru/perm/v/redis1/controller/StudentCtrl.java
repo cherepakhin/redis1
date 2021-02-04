52880b98ca82cb8f7ed0e30199400adf18febdc1
Черепахин Василий Геннадьевич <vasilii.cherepakhin@domru.ru>
2021-02-04 17:57:30 +0500

    Добавлена web форма для ввода Student

---
+++ src/main/java/ru/perm/v/redis1/controller/StudentCtrl.java
@@ -0,0 +1,43 @@
+package ru.perm.v.redis1.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.perm.v.redis1.model.Student;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("")
+public class StudentCtrl {
+
+    private static final Logger LOG = LoggerFactory.getLogger(StudentCtrl.class);
+    private static final String STUDENT_PAGE = "studentpage";
+    private static final String STUDENT_ATTR = "student";
+
+    @GetMapping("/")
+    public String start(Model model) {
+        Student student = new Student();
+        model.addAttribute(STUDENT_ATTR, student);
+        return STUDENT_PAGE;
+    }
+
+    @PostMapping("/")
+    public String save(
+            @ModelAttribute(STUDENT_ATTR) @Valid Student student,
+            BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute(STUDENT_ATTR, student);
+            return STUDENT_PAGE;
+        }
+        LOG.info(student.toString());
+        model.addAttribute(STUDENT_ATTR, student);
+        return STUDENT_PAGE;
+    }
+}
--- src/main/java/ru/perm/v/redis1/model/Student.java
+++ src/main/java/ru/perm/v/redis1/model/Student.java
@@ -1,25 +1,58 @@
 package ru.perm.v.redis1.model;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 import org.springframework.data.redis.core.RedisHash;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
-@Getter
-@Setter
-@EqualsAndHashCode
-@ToString
 @RedisHash("Student")
 public class Student implements Serializable {
-    public enum Gender {
-        MALE, FEMALE
+    @Min(value = 1, message = "Id should not be less than 1")
+    private Integer id;
+    @Size(min = 3, max = 200, message
+            = "Name must be between 3 and 200 characters")
+    private String name;
+
+    public Integer getId() {
+        return id;
     }
 
-    private String id;
-    private String name;
-    private Gender gender;
-    private int grade;
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+
+        Student student = (Student) o;
+
+        if (id != null ? !id.equals(student.id) : student.id != null) return false;
+        return name != null ? name.equals(student.name) : student.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
