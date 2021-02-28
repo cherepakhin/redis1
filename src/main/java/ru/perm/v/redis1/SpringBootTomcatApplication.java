c83c69f3f88fb421014593b06214219a4d4b8ed0
Черепахин Василий Геннадьевич <vasi.che@gmail.com>
2021-02-28 12:44:41 +0500

    Создание war

---
+++ src/main/java/ru/perm/v/redis1/SpringBootTomcatApplication.java
@@ -0,0 +1,8 @@
+package ru.perm.v.redis1;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootTomcatApplication extends SpringBootServletInitializer {
+}
