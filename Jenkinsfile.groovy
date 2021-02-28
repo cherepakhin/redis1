60020f5c655c6a3f775af279e9d39981f20b23fd
Черепахин Василий Геннадьевич <vasi.che@gmail.com>
2021-02-28 23:22:18 +0500

    Jenkinsfile -> Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,45 @@
+pipeline {
+    agent any
+
+    options {
+        skipStagesAfterUnstable()
+        skipDefaultCheckout()
+    }
+
+    environment {
+        //Use Pipeline Utility Steps plugin to read information from pom.xml into env variables
+        IMAGE = readMavenPom().getArtifactId()
+        VERSION = readMavenPom().getVersion()
+    }
+
+    stages {
+        stage('Build') {
+            steps {
+                checkout scm
+                sh 'mvn compile'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'mvn test'
+                junit '**/target/surefire-reports/TEST-*.xml'
+            }
+        }
+        stage('Package') {
+            steps {
+                sh 'mvn package -DskipTests'
+            }
+            post {
+                success {
+                    archiveArtifacts 'target/*.war'
+                }
+            }
+        }
+        stage('Deploy') {
+            steps {
+                mail(to: 'vasi.che@gmail.com', subject: "Успешная сборка: ${currentBuild.fullDisplayName}", body: "Ссылка на результат ${env.BUILD_URL}")
+                sh "curl -T \"target/redis1##${VERSION}.war\" " + "\"http://deployer:pass@v.perm" + ".ru:8080/manager/text/deploy?path=/redis1&update=true\""
+            }
+        }
+    }
+}
