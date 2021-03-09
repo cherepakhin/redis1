b24f4fc7db525f77aaea087ed30c6eb1872da713
Черепахин Василий Геннадьевич <vasi.che@gmail.com>
2021-03-09 13:10:20 +0500

    Package to jar for docker push

--- src/main/java/ru/perm/v/redis1/Redis1Application.java
+++ src/main/java/ru/perm/v/redis1/Redis1Application.java
@@ -3,7 +3,7 @@ package ru.perm.v.redis1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class Redis1Application {
 
     public static void main(String[] args) {
--- src/main/java/ru/perm/v/redis1/SpringBootTomcatApplication.java
+++ src/main/java/ru/perm/v/redis1/SpringBootTomcatApplication.java
@@ -3,6 +3,6 @@ package ru.perm.v.redis1;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SpringBootTomcatApplication extends SpringBootServletInitializer {
 }
